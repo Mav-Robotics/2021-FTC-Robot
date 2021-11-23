ad563b0cca4520a3fce4ad6341c17d656d7c6b1a
David Curran <currand@gmail.com>
2021-11-23 10:39:24 -0500

    Auto menus

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/StrafeDistance.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.DrivetrainMecanum;
+
+public class StrafeDistance extends CommandBase {
+    private DrivetrainMecanum m_drivetrain;
+    private Telemetry m_telemetry;
+    private Double m_speed;
+    private Double m_distance;
+
+    public StrafeDistance(DrivetrainMecanum drivetrain, Double speed, Double distance, Telemetry telemetry) {
+        m_drivetrain = drivetrain;
+        m_telemetry = telemetry;
+        m_speed = speed;
+        m_distance = distance;
+        addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        m_drivetrain.resetEncoders();
+        m_drivetrain.resetHeading();
+    }
+
+    @Override
+    public void execute() {
+        if (m_distance >= 0.0) {
+            m_drivetrain.drive(0, -m_speed, 0);
+        } else {
+            m_drivetrain.drive(0, m_speed, 0);
+        }
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (m_distance >= 0.0) {
+            return (m_drivetrain.getAverageDistance() >= m_distance);
+        } else {
+            return (m_drivetrain.getAverageDistance() <= m_distance);
+        }
+    }
+}
