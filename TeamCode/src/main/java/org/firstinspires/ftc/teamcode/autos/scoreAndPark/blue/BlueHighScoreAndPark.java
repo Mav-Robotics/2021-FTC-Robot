a3f9c4a3b5c811cc416cc654d119aa571e5f455e
PeterGargas <gargaspeter1@gmail.com>
2021-12-11 07:56:32 -0500

    made changes in the autos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/scoreAndPark/blue/BlueHighScoreAndPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/scoreAndPark/blue/BlueHighScoreAndPark.java
@@ -22,9 +22,9 @@ public class BlueHighScoreAndPark extends SequentialCommandGroup {
                 new DriveDistance(drivetrain, 0.6, 24.5, telemetry).whenFinished(() -> drivetrain.stopAll()),
                 new IntakeOut(intake, telemetry).withTimeout(2000).whenFinished(() -> intake.stopIntake()),
                 new DriveDistance(drivetrain, 0.6, -5.0, telemetry).whenFinished(() -> drivetrain.stopAll()),
-                new ArmToPosition(arm, RobotMap.PICKUP, telemetry).whenFinished(() -> arm.stopAll()),
+                new ArmToPosition(arm, RobotMap.LOW_TARGET, telemetry).whenFinished(() -> arm.stopAll()),
                 new TurnToAngle(drivetrain, 90.0, 0.5),
-                new DriveDistance(drivetrain, 0.6, -54.0, telemetry).whenFinished(() -> drivetrain.stopAll()),                new StrafeDistance(drivetrain, 0.6, 10.0, "LEFT", telemetry).whenFinished(() -> drivetrain.stopAll()),
+                new DriveDistance(drivetrain, 0.6, -54.0, telemetry).whenFinished(() -> drivetrain.stopAll()),            new StrafeDistance(drivetrain, 0.6, 10.0, "LEFT", telemetry).whenFinished(() -> drivetrain.stopAll()),
                 new StrafeDistance(drivetrain, 0.6, 10.0, "LEFT", telemetry).whenFinished(() -> drivetrain.stopAll())
         );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/scoreAndPark/red/RedHighScoreAndPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/scoreAndPark/red/RedHighScoreAndPark.java
@@ -21,12 +21,12 @@ public class RedHighScoreAndPark extends SequentialCommandGroup {
         addCommands(
                 new ArmToPosition(arm, RobotMap.HI_TARGET, telemetry),
                 new TurnToAngle(drivetrain, 9.0, 0.5),
-                new DriveDistance(drivetrain, 0.6, 24.5, telemetry).whenFinished(() -> drivetrain.stopAll()),
+                new DriveDistance(drivetrain, 0.6, 23.5, telemetry).whenFinished(() -> drivetrain.stopAll()),
                 new IntakeOut(intake, telemetry).withTimeout(2000).whenFinished(() -> intake.stopIntake()),
-                new DriveDistance(drivetrain, 0.6, -5.0, telemetry).whenFinished(() -> drivetrain.stopAll()),
+                new DriveDistance(drivetrain, 0.6, -7.0, telemetry).whenFinished(() -> drivetrain.stopAll()),
                 new ArmToPosition(arm, RobotMap.PICKUP, telemetry).whenFinished(() -> arm.stopAll()),
                 new TurnToAngle(drivetrain, -90.0, 0.5),
-                new DriveDistance(drivetrain, 0.6, -54.0, telemetry).whenFinished(() -> drivetrain.stopAll()),                new StrafeDistance(drivetrain, 0.6, 10.0, "LEFT", telemetry).whenFinished(() -> drivetrain.stopAll()),
+                new DriveDistance(drivetrain, 0.6, -54.0, telemetry).whenFinished(() -> drivetrain.stopAll()),
                 new StrafeDistance(drivetrain, 0.6, 10.0, "LEFT", telemetry).whenFinished(() -> drivetrain.stopAll())
         );
 
